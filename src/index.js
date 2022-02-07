fdf7accc23f773c9430f4b3963c7547414cb5f5c
Matt Eilar <meilar@gmail.com>
2022-02-07 10:44:14 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import testCode from './test-code.js';
 
-$(document).ready(function() {
-  let msg = testCode();
-  console.log("This page is loading correctly " + msg);
+$(function() {
+ $('#weatherLocation').click(function() {
+  const city = $("#location").val();
+  $("#location").val("");
+  
+  let request = new XMLHttpRequest();
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=portland,oregon&appid=${process.env.API_KEY}`;
+  console.log(url);
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    } 
+  };
+
+  request.open("GET", url, true);
+  request.send();
+
+  function getElements(response) {
+    $(".showHumidity").text("The humidity in ${city} is ${response.main.humidity}%");
+    $(".showTemp").text("The temperature in Kelvins is ${response.main.temp} degrees.");
+  }
+ });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: './src/index.js',
@@ -14,8 +15,9 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new Dotenv(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'API-practice',
       template: './src/index.html',
       inject: 'body'
     })
